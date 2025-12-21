0b0ec1e7114a2f78b475f844dc248fd71e2da08e
daniellauding <daniel@lauding.se>
2025-12-21 14:29:25 +0100

    Initial Commit

---
+++ Instinctly/InstinctlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstinctlyApp.swift
+//  Instinctly
+//
+//  Created by Daniel Lauding on 2025-12-21.
+//
+
+import SwiftUI
+
+@main
+struct InstinctlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
